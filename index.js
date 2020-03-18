99b55e31c8f5229e9cb856dc699c607a37188a2e
amnyrrd <amnyrrd@gmail.com>
2020-03-18 13:19:37 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+const superbowlWin = (record) => {
+  let win = record.find( record => record.result === 'W' )
+  return !!win ? win.year : undefined
+}
